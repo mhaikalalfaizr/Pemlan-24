b22e47612af3660cd56223d08f6438aee13e9bb3
faiz <160980089+mhaikalalfaizr@users.noreply.github.com>
2024-03-25 23:45:47 +0700

    file Class untuk menyimpan constructor

---
+++ Vet_v_2_0/pantheraTigris.java
@@ -0,0 +1,18 @@
+package tugas01;
+    
+    class Harimau{
+    
+        String id;
+        String nama;
+        String warna;
+        int tahunLahir;
+
+        void rawr(){
+            System.out.println("Rawrrrrrr");
+        }
+      
+    
+    }
+    
+
+
